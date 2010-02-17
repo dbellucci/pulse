fec195fa41d47720da65bbde0b1d81eed57eb28e
Daniele Bellucci <daniele.bellucci@gmail.com>
2010-02-17 17:51:33 +0100

    fixing aliasing issues

---
+++ lib/pulsedb.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'sqlite3'
+
+module PulseDB
+	def db_report(value)
+		@db_handle.execute("INSERT INTO sample(sample) VALUES ('#{value.to_s}')")
+	end
+	
+	def db_open(name)
+		@db_handle = SQLite3::Database.new(name)
+		
+		at_exit { 
+			db_close 
+		}
+		
+		@db_handle.execute("CREATE TABLE sample (id INTEGER PRIMARY KEY, sample TEXT, timestamp DEFAULT CURRENT_TIMESTAMP)")
+	end
+
+	def db_close
+		@db_handle.close
+	end
+end
+
